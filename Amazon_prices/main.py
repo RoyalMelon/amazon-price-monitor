929207ce7fdcd12d335a816c9666ba9874bc6423
RoyalMelon <109209370+RoyalMelon@users.noreply.github.com>
2022-07-17 14:10:40 +0100

    Update main.py

--- Amazon_prices/main.py
+++ Amazon_prices/main.py
@@ -2,6 +2,8 @@ from data import data
 from utilities import Help as help
 from datetime import datetime
 
+from time import sleep
+
 
 def main():
     while True:
@@ -13,11 +15,9 @@ def main():
                 for index in range(len(item)):
                     help.is_sale(item[index])
 
-        # loops throught the values 0-12 in steps of twos
-        # So that program runs every two hours
-        for hour in range(0, 14, 2):
-            if time == f'{hour}:00:00':
-                run_program()
+        # Program runs every two hours
+        run_program()
+        sleep(7200)
 
 
 if __name__ == '__main__':
