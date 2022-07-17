efa98aaf4b06eba13909d02b0c23a90fbc44e1c3
RoyalMelon <109209370+RoyalMelon@users.noreply.github.com>
2022-07-17 14:11:28 +0100

    Update main.py

--- Amazon_prices/main.py
+++ Amazon_prices/main.py
@@ -1,14 +1,11 @@
 from data import data
 from utilities import Help as help
-from datetime import datetime
 
 from time import sleep
 
 
 def main():
     while True:
-        time = datetime.now()
-        time = time.strftime("%H:%M:%S")
 
         def run_program():
             for item in data:
