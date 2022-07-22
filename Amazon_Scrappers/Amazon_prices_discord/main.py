3de5c773e441df870d5d34e10320f6fbd26c8aaf
RoyalMelon <109209370+RoyalMelon@users.noreply.github.com>
2022-07-22 10:00:20 +0100

    Update main.py

--- Amazon_Scrappers/Amazon_prices_discord/main.py
+++ Amazon_Scrappers/Amazon_prices_discord/main.py
@@ -8,32 +8,33 @@ import time
 
 
 def main():
+
+    def discord_send(info):
+        dotenv.load_dotenv()
+        webhook_url = os.getenv('URL')
+        webhook = DiscordWebhook(url=webhook_url)
+
+        description = f'Price: £{info[1]}\nOriginal Price: £{info[2]}\nDiscount: -{info[3]}%'
+        # create embed object for webhook
+        embed = DiscordEmbed(
+            title=info[0], description=description, url=info[4], color='03b2f8')
+
+        # add embed object to webhook
+        webhook.add_embed(embed)
+
+        webhook.execute()
+
+    def run_program():
+        for item in data:
+            for index in range(len(item)):
+                info = help.is_sale(item[index])
+                if info != None:
+                    discord_send(info)
+    
+    one_day = 86400 #seconds
     while True:
-        def discord_send(info):
-            dotenv.load_dotenv()
-            webhook_url = os.getenv('URL')
-            webhook = DiscordWebhook(url=webhook_url)
-
-            description = f'Price: £{info[1]}\nOriginal Price: £{info[2]}\nDiscount: -{info[3]}%'
-            # create embed object for webhook
-            embed = DiscordEmbed(
-                title=info[0], description=description, url=info[4], color='03b2f8')
-
-            # add embed object to webhook
-            webhook.add_embed(embed)
-
-            webhook.execute()
-
-        def run_program():
-            for item in data:
-                for index in range(len(item)):
-                    info = help.is_sale(item[index])
-                    if info != None:
-                        discord_send(info)
-
         run_program()
-        # Runs every 2 hours
-        time.sleep(7200)
+        time.sleep(one_day)
 
 
 if __name__ == '__main__':
